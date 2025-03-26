1b41c835de7b8e9fc0237cec927b0b79113f2ec9
hs-2171073-LeeKyungseok <115882994+hs-2171073-LeeKyungseok@users.noreply.github.com>
2025-03-26 20:07:33 +0900

    Title: 정수 선언하고 뺄셈, Time: 90ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250326/정수 선언하고 뺄셈/define-numbers-and-substract.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 97;
+        int b = 13;
+        int num = a - b;
+        System.out.println(a + " - " + b + " = " + num);
+    }
+}
