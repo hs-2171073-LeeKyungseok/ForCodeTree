85a4edd006cfc022b443e5f6c2bbcdbe35183af8
hs-2171073-LeeKyungseok <115882994+hs-2171073-LeeKyungseok@users.noreply.github.com>
2025-03-26 20:02:41 +0900

    Title: 두줄 출력, Time: 93ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250326/두줄 출력/print-two-sentences-introduce.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void printHello() {
+        System.out.println("Hello students!");
+        System.out.println("Welcome to LeebrosCode!");
+    }
+    public static void main(String[] args) {
+        printHello();
+    }
+}
