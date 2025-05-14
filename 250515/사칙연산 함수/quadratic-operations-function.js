e05ecde2a0cce0f696088f8e14b6fcab990508bb
hs-2171073-LeeKyungseok <115882994+hs-2171073-LeeKyungseok@users.noreply.github.com>
2025-05-15 01:52:20 +0900

    Title: 사칙연산 함수, Time: 189ms, Memory: 15MB, Status: Passed - Codetree

--- 250515/사칙연산 함수/quadratic-operations-function.js
+++ 250515/사칙연산 함수/quadratic-operations-function.js
@@ -4,20 +4,15 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 let [a, o, c] = input[0].split(" ");
 a = Number(a);
 c = Number(c);
-// Please Write your code here.
 
-    if(o.toString() == "+") {
-        console.log(a + " + " + c + " = " + (a+c))
-    }
-    else if(o.toString() == "-") {
-        console.log(a + " - " + c + " = " + (a-c))
-    }
-    else if(o.toString() == "*") {
-        console.log(a + " * " + c + " = " + (a*c))
-    }
-    else if(o.toString() == "/") {
-        console.log(a + " / " + c + " = " + (a/c))
-    }
-    else {
-        console.log("False")
-    }
+if (o === "+") {
+    console.log(`${a} + ${c} = ${a + c}`);
+} else if (o === "-") {
+    console.log(`${a} - ${c} = ${a - c}`);
+} else if (o === "*") {
+    console.log(`${a} * ${c} = ${a * c}`);
+} else if (o === "/") {
+    console.log(`${a} / ${c} = ${Math.floor(a / c)}`);  // 정수 부분만 출력
+} else {
+    console.log("False");
+}
