514ebd3c6e2d092848cb5c3b1fbd2f74c8191c17
hs-2171073-LeeKyungseok <115882994+hs-2171073-LeeKyungseok@users.noreply.github.com>
2025-10-23 01:08:01 +0900

    Title: 길이 단위 변환하기, Time: 62ms, Memory: 10MB, Status: Passed - Codetree

--- 251023/길이 단위 변환하기/change-length-unit.js
+++ 251023/길이 단위 변환하기/change-length-unit.js
@@ -1,2 +1,11 @@
 const CM_PER_FT = 30.48;
 const CM_PER_MI = 160934;
+
+let ft = 9.2;
+let mi = 1.3;
+
+let ftToCm = ft * CM_PER_FT;
+let miToCm = mi * CM_PER_MI;
+
+console.log(ft + "ft = " + ftToCm.toFixed(1) + "cm");
+console.log(mi + "mi = " + miToCm.toFixed(1) + "cm");
